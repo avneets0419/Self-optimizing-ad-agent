8e255cac319cf5c5bf9f9569ab13324404d70791
avneets0419 <avneet0419@gmail.com>
2026-03-17 02:13:28 +0530

    Intialized Image analyzer

---
+++ image_analyzer.py
@@ -0,0 +1,10 @@
+def analyze_image(image_path, product_description):
+
+    print("\nAnalyzing ad creative...")
+
+    analysis = {
+        "image_theme": "fitness motivation",
+        "product": product_description
+    }
+
+    return analysis
